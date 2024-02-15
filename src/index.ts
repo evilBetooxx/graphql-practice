8d8bfcfff5e7a5e391efc55ab0f89d7a6536455b
Daniel Fernando Pérez Sarmiento <daniel.perez@cpavs.mx>
2024-02-15 00:45:46 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+const books = [
+    {
+        id: 1,
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+        stock: 2
+    },
+    {
+        id: 2,
+        title: 'City of Glass',
+        author: 'Paul Auster',
+        stock: 13
+    },
+    {
+        id: 3,
+        title: "The Great Gatsby",
+        author: 'Paul Auster',
+        stock: 23
+    },
+];
+
+console.log("OK!")
